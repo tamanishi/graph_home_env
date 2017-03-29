27806b812f8ee181283d712d608513fa466ac38b
tamanishi <tamanishi@gmail.com>
2017-03-29 15:05:56 +0900

    environment values to dotenv

--- main.js
+++ main.js
@@ -1,19 +1,37 @@
+require('dotenv').config();
 const gs = require('google-spreadsheet');
-const app = require('express')();
+const express = require('express');
+const app = express();
 
-const my_book = new gs('18PygxLjeC2AvxMe-vW1vG1V0M8TcrSV7-ycGm4FSPws');
-const credentials = require(__dirname + '/gs_credentials.json');
+const my_book = new gs(process.env.gs_id);
+// const credentials = require(__dirname + '/gs_credentials.json');
+const credentials = JSON.parse(process.env.gs_credentials);
 
-app.get('/', (req, res) => {
-  let my_sheet;
+app.get('/lastday', (req, res) => {
   my_book.useServiceAccountAuth(credentials, (err) => {
     my_book.getInfo( (err, data) => {
-      my_sheet = data.worksheets[0];
-      console.log(my_sheet);
-      res.send(JSON.stringify(my_sheet));
+      let my_sheet = data.worksheets[0];
+      // console.log(my_sheet);
+      const row_count = my_sheet.rowCount;
+      var result = [];
+      my_sheet.getRows({offset: row_count - 96 > 0 ? row_count - 96 : 0, limit: 96}, (err, rows) => {
+        for (let row of rows) {
+          let elm = {
+            timestamp: row.timestamp,
+            temperature: row.temperature,
+            pressure: row.pressure,
+            humidity: row.humidity
+          };
+          result.push(elm);
+        }
+        res.header('Content-Type', 'application/json');
+        res.send(JSON.stringify(result));
+      });
     });
   });
 });
+
+app.use(express.static('pages'));
 
 app.listen(3000);
 
---
+++ pages/render.js
@@ -0,0 +1,71 @@
+var request = window.superagent;
+
+request
+  .get('/lastday')
+  .end( (err, res) => {
+    // console.log(res.text);
+    let rawData = JSON.parse(res.text);
+    var temperatureData = rawData.map( (e, i, a) => {
+      let point = {
+        x: new Date(e.timestamp),
+        y: Number(e.temperature)
+      };
+      return point;
+    });
+    // console.log(JSON.stringify(temperatureData));
+    var temperatureChart = new CanvasJS.Chart('temperature', {
+      title: {
+        text: "temperature"
+      },
+      data: [{
+        type: 'line',
+        color: 'blue',
+        dataPoints: temperatureData 
+      }]
+    });
+    temperatureChart.render();
+
+    var humidityData = rawData.map( (e, i, a) => {
+      let point = {
+        x: new Date(e.timestamp),
+        y: Number(e.humidity)
+      };
+      return point;
+    });
+    // console.log(JSON.stringify(humidityData));
+    var humidityChart = new CanvasJS.Chart('humidity', {
+      title: {
+        text: "humidity"
+      },
+      data: [{
+        type: 'line',
+        color: 'red',
+        dataPoints: humidityData 
+      }]
+    });
+    humidityChart.render();
+
+    var pressureData = rawData.map( (e, i, a) => {
+      let point = {
+        x: new Date(e.timestamp),
+        y: Number(e.pressure)
+      };
+      return point;
+    });
+    // console.log(JSON.stringify(pressureData));
+    var pressureChart = new CanvasJS.Chart('pressure', {
+      title: {
+        text: "pressure"
+      },
+      axisY: {
+        includeZero: false
+      },
+      data: [{
+        type: 'line',
+        color: 'green',
+        dataPoints: pressureData 
+      }]
+    });
+    pressureChart.render();
+  });
+
