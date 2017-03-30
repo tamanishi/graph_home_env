f3bdbcf06b8aeb8be3a6924ab1c8c885ceaf2212
tamanishi <tamanishi@gmail.com>
2017-03-30 20:09:30 +0900

    refresh graphs per 15 minutes

--- pages/render.js
+++ pages/render.js
@@ -1,77 +1,82 @@
 var request = window.superagent;
 
-request
-  .get('/lastday')
-  .end( (err, res) => {
-    // console.log(res.text);
-    let rawData = JSON.parse(res.text);
-    var temperatureData = rawData.map( (e, i, a) => {
-      let point = {
-        x: new Date(e.timestamp),
-        y: Number(e.temperature)
-      };
-      return point;
-    });
-    // console.log(JSON.stringify(temperatureData));
-    var temperatureChart = new CanvasJS.Chart('temperature', {
-      title: {
-        text: "temperature"
-      },
-      axisY: {
-        includeZero: false
-      },
-      data: [{
-        type: 'line',
-        color: 'blue',
-        dataPoints: temperatureData
-      }]
-    });
-    temperatureChart.render();
+function render() {
+  request
+    .get('/lastday')
+    .end( (err, res) => {
+      // console.log(res.text);
+      let rawData = JSON.parse(res.text);
+      var temperatureData = rawData.map( (e, i, a) => {
+        let point = {
+          x: new Date(e.timestamp),
+          y: Number(e.temperature)
+        };
+        return point;
+      });
+      // console.log(JSON.stringify(temperatureData));
+      var temperatureChart = new CanvasJS.Chart('temperature', {
+        title: {
+          text: "temperature"
+        },
+        axisY: {
+          includeZero: false
+        },
+        data: [{
+          type: 'line',
+          color: 'blue',
+          dataPoints: temperatureData
+        }]
+      });
+      temperatureChart.render();
 
-    var humidityData = rawData.map( (e, i, a) => {
-      let point = {
-        x: new Date(e.timestamp),
-        y: Number(e.humidity)
-      };
-      return point;
-    });
-    // console.log(JSON.stringify(humidityData));
-    var humidityChart = new CanvasJS.Chart('humidity', {
-      title: {
-        text: "humidity"
-      },
-      axisY: {
-        includeZero: false
-      },
-      data: [{
-        type: 'line',
-        color: 'red',
-        dataPoints: humidityData
-      }]
-    });
-    humidityChart.render();
+      var humidityData = rawData.map( (e, i, a) => {
+        let point = {
+          x: new Date(e.timestamp),
+          y: Number(e.humidity)
+        };
+        return point;
+      });
+      // console.log(JSON.stringify(humidityData));
+      var humidityChart = new CanvasJS.Chart('humidity', {
+        title: {
+          text: "humidity"
+        },
+        axisY: {
+          includeZero: false
+        },
+        data: [{
+          type: 'line',
+          color: 'red',
+          dataPoints: humidityData
+        }]
+      });
+      humidityChart.render();
 
-    var pressureData = rawData.map( (e, i, a) => {
-      let point = {
-        x: new Date(e.timestamp),
-        y: Number(e.pressure)
-      };
-      return point;
+      var pressureData = rawData.map( (e, i, a) => {
+        let point = {
+          x: new Date(e.timestamp),
+          y: Number(e.pressure)
+        };
+        return point;
+      });
+      // console.log(JSON.stringify(pressureData));
+      var pressureChart = new CanvasJS.Chart('pressure', {
+        title: {
+          text: "pressure"
+        },
+        axisY: {
+          includeZero: false
+        },
+        data: [{
+          type: 'line',
+          color: 'green',
+          dataPoints: pressureData
+        }]
+      });
+      pressureChart.render();
     });
-    // console.log(JSON.stringify(pressureData));
-    var pressureChart = new CanvasJS.Chart('pressure', {
-      title: {
-        text: "pressure"
-      },
-      axisY: {
-        includeZero: false
-      },
-      data: [{
-        type: 'line',
-        color: 'green',
-        dataPoints: pressureData
-      }]
-    });
-    pressureChart.render();
-  });
+}
 
+setTimeout(function() {
+  render();
+}, 15 * 60 * 1000);
