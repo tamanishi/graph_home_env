c088bb3f06cd01d1d992e9a590004943aafbba3c
Masayuki Sunahara <tamanishi@gmail.com>
2018-07-29 07:18:47 +0900

    apply dark theme

--- pages/render.js
+++ pages/render.js
@@ -30,9 +30,15 @@ function render() {
       });
 
       Highcharts.chart('container', {
+        chart: {
+          backgroundColor: '#222222'
+        },
 
         title: {
-          text: 'home environment (last 24 hours)'
+          text: 'home environment (last 24 hours)',
+          style: {
+            color: '#eeeeee'
+          }
         },
 
         // subtitle: {
@@ -49,132 +55,140 @@ function render() {
             week: '%Y/%m/%d',
             month: '%Y/%m',
             year: '(%Y)'
+          },
+          labels: {
+            style: {
+              color: '#eeeeee'
+            }
           }
         },
 
         yAxis: [
-          {
-            // className: "highcharts-color-0",
-            title: {
-              text: "temperature",
-              style: {
-                color: "#f18c16"
-              }
-            },
-            labels: {
-              format: "{value}℃",
-              style: {
-                color: "#f18c16"
-              }
-            },
-            ceiling: 35,
-            floor: 0,
-          },
-          {
-            gridLineWidth: 0,
-            title: {
-              text: 'humidity',
-              style: {
-                color: "#7cb5ec"
-              }
-            },
-            labels: {
-              format: "{value} %",
-              style: {
-                color: "#7cb5ec"
-              }
-            },
-            ceiling: 80,
-            floor: 20,
-            opposite: true
-          },
-          {
-            gridLineWidth: 0,
-            title: {
-              text: 'pressure',
-              style: {
-                color: "#90ed7d"
-              }
-            },
-            labels: {
-              format: "{value} hPa",
-              style: {
-                color: "#90ed7d"
-              }
-            },
-            ceiling: 1300,
-            floor: 800,
-            opposite: true
-          }
-        ],
-
-        legend: {
-          layout: 'vertical',
-          align: 'left',
-          verticalAlign: 'top',
-          floating: true,
-          x: 80,
-          y: 40
-        },
-
-        plotOptions: {
-          series: {
-            marker: {
-              enabled: false
+        {
+          // className: "highcharts-color-0",
+          title: {
+            text: "temperature",
+            style: {
+              color: "#f18c16"
             }
-            //   label: {
-            //     connectorAllowed: false
-            //   }
-          }
-        },
-
-        series: [
-          {
-            name: 'temperature',
-            data: temperatureData,
-            type: 'spline',
-            color: '#f18c16',
-            pointStart: startDateTime,
-            pointInterval: 60 * 15 * 1000,
-            yAxis: 0
           },
-          {
-            name: 'humidity',
-            data: humidityData,
-            type: 'spline',
-            color: '#7cb5ec',
-            pointStart: startDateTime,
-            pointInterval: 60 * 15 * 1000,
-            yAxis: 1
-          },
-          {
-            name: 'pressure',
-            data: pressureData,
-            type: 'spline',
-            color: '#90ed7d',
-            pointStart: startDateTime,
-            pointInterval: 60 * 15 * 1000,
-            yAxis: 2
-          }
-        ],
-
-        responsive: {
-          rules: [{
-            condition: {
-              maxWidth: 500
-            },
-            chartOptions: {
-              legend: {
-                layout: 'horizontal',
-                align: 'center',
-                verticalAlign: 'bottom'
-              }
+          labels: {
+            format: "{value}℃",
+            style: {
+              color: "#f18c16"
             }
-          }]
+          },
+          ceiling: 35,
+          floor: 0,
+        },
+        {
+          gridLineWidth: 0,
+          title: {
+            text: 'humidity',
+            style: {
+              color: "#7cb5ec"
+            }
+          },
+          labels: {
+            format: "{value} %",
+            style: {
+              color: "#7cb5ec"
+            }
+          },
+          ceiling: 80,
+          floor: 20,
+          opposite: true
+        },
+        {
+          gridLineWidth: 0,
+          title: {
+            text: 'pressure',
+            style: {
+              color: "#90ed7d"
+            }
+          },
+          labels: {
+            format: "{value} hPa",
+            style: {
+              color: "#90ed7d"
+            }
+          },
+          ceiling: 1300,
+          floor: 800,
+          opposite: true
         }
+      ],
 
-      });
+      legend: {
+        layout: 'vertical',
+        align: 'left',
+        verticalAlign: 'top',
+        floating: true,
+        x: 80,
+        y: 40,
+        itemStyle: {
+          color: '#eeeeee'
+        }
+    },
+
+      plotOptions: {
+        series: {
+          marker: {
+            enabled: false
+          }
+          //   label: {
+          //     connectorAllowed: false
+          //   }
+        }
+      },
+
+      series: [
+        {
+          name: 'temperature',
+          data: temperatureData,
+          type: 'spline',
+          color: '#f18c16',
+          pointStart: startDateTime,
+          pointInterval: 60 * 15 * 1000,
+          yAxis: 0
+        },
+        {
+          name: 'humidity',
+          data: humidityData,
+          type: 'spline',
+          color: '#7cb5ec',
+          pointStart: startDateTime,
+          pointInterval: 60 * 15 * 1000,
+          yAxis: 1
+        },
+        {
+          name: 'pressure',
+          data: pressureData,
+          type: 'spline',
+          color: '#90ed7d',
+          pointStart: startDateTime,
+          pointInterval: 60 * 15 * 1000,
+          yAxis: 2
+        }
+      ],
+
+      responsive: {
+        rules: [{
+          condition: {
+            maxWidth: 500
+          },
+          chartOptions: {
+            legend: {
+              layout: 'horizontal',
+              align: 'center',
+              verticalAlign: 'bottom'
+            }
+          }
+        }]
+      }
+
     });
+  });
 }
 
 render();
